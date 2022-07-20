315d51ea53bffc8b0de5ffa2f8d845304c06f1c2
cavilanr <c.avilanr@uniandes.edu.co>
2022-07-20 16:24:25 -0500

    update scripts

--- script/taller3.R
+++ script/taller3.R
@@ -10,8 +10,10 @@ p_load(tidyverse,rio,
        osmdata,
        spdep,
        secr,
+       osmdata,
        here) # Get OSM's data
 path = here('')
+
 
 train<-readRDS(here(path,"stores/train.Rds"))
 test<-readRDS(here(path,"stores/test.Rds"))
@@ -272,54 +274,416 @@ leaflet() %>% addTiles() %>%
 test_final<-rbind(test_bog,test_med)  
 
 
+
 #Solo Chapinero
 
-cha.poly<-st_read(here(path,"stores/localidades.shp"))
-class(cha.poly)
-st_geometry(cha.poly[13,])
+mz_bog<-st_read(here(path,"11_BOGOTA/URBANO/MGN_URB_MANZANA.shp"))
 
-st_crs(cha.poly[13,])
-cha.poly[13,]%>% head()
-cha.poly<-st_transform(cha.poly,"WGS84") 
-st_crs(cha.poly)
-st_crs(cha.poly[13,])
-cha.poly[13,]%>% head()
-leaflet() %>% addTiles() %>% 
-  addPolygons(data=cha.poly[13,])
 
-lst = st_intersects (train_final,cha.poly[13,])
-land = lengths(st_intersects(train_final,cha.poly[13,])) > 0
-land=as.data.frame(land)
-train_final$chapi<-NULL
-train_final$chapi<-land[,1]
+chapinero = getbb(place_name = "UPZ Chapinero, Bogota",
+                  featuretype = "boundary:administrative",
+                  format_out = "sf_polygon") %>% .$multipolygon
 
-train_chap<-filter(train_final,chapi==TRUE)
+mz_chapi = mz_bog[chapinero,]
+train_chapi = train_final[chapinero,]
 
-leaflet() %>% addTiles()  %>% 
-  addCircleMarkers(data=train_chap, col="blue")%>% 
-  addPolygons(data=cha.poly[13,])
 
-#Solo Medellín
+leaflet() %>% addTiles()%>%addCircleMarkers(data=train_chapi, col="red") %>% 
+  addPolygons(data=mz_chapi) 
 
-med.poly<-st_read(here(path,"stores/LimiteComunaCorregimiento_2014.shp"))
-med.poly<-st_transform(med.poly,"WGS84") 
-pol_poblad <-st_cast(med.poly[14,], "POLYGON")
-class(pol_poblad)
-st_geometry(pol_poblad)
-st_crs(pol_poblad)
-pol_poblad%>% head()
+## Prediccion de superficie a partir de la descripcion
 
-leaflet() %>% addTiles() %>% 
-  addPolygons(data=pol_poblad)
+table(is.na(train_chapi$surface_total))
+table(is.na(train_chapi$surface_covered))
 
-lst = st_intersects (train_final,pol_poblad)
-land = lengths(st_intersects(train_final,pol_poblad)) > 0
-land=as.data.frame(land)
-train_final$med<-NULL
-train_final$med<-land[,1]
+train_chapi = train_chapi %>% mutate(new_surface=surface_total)
+train_chapi = train_chapi %>% mutate(new_surface=ifelse(is.na(new_surface)==T,surface_covered,new_surface))
 
-train_med<-filter(train_final,med==TRUE)
+table(is.na(train_chapi$new_surface))
 
-leaflet() %>% addTiles()  %>% 
-  addCircleMarkers(data=train_med, col="blue")%>% 
-  addPolygons(data=pol_poblad)
+train_chapi = train_chapi %>% mutate(new_desc=str_to_lower(description))
+
+
+p1 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m" ## pattern
+p2 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m2" ## pattern
+
+train_chapi$new_surface2<-NA
+
+train_chapi = train_chapi %>% 
+  mutate(new_surface2 = str_extract(string=train_chapi$new_desc , pattern= p1))
+train_chapi = train_chapi %>% 
+  mutate(new_surface2 = ifelse(is.na(new_surface2)==T,
+                               str_extract(string=train_chapi$new_desc , pattern= p2),
+                               new_surface2))
+train_chapi$new_surface3<-NA
+train_chapi = train_chapi %>% 
+  mutate(new_surface3 = str_extract(string=train_chapi$new_surface2 , pattern= "[:digit:]{2,3}"))
+train_chapi$new_surface3 <- as.numeric(train_chapi$new_surface3)
+train_chapi = train_chapi %>% mutate(new_surface3 = ifelse(new_surface3<=40,NA,new_surface3))
+
+train_chapi = train_chapi %>% mutate(new_surface = ifelse(is.na(new_surface)==T,new_surface3,new_surface))
+table(is.na(train_chapi$new_surface))
+
+## Prediccion de superficie a partir de las manzanas
+
+house_mnz = st_join(x = train_chapi,y = mz_chapi)
+colnames(house_mnz)
+house_mnz = house_mnz %>%
+  group_by(MANZ_CCNCT) %>%
+  mutate(new_surface_mzn=median(new_surface,na.rm=T))
+
+train_chapi = train_chapi %>% mutate(new_surface = ifelse(is.na(new_surface)==T,house_mnz$new_surface_mzn,new_surface))
+
+table(is.na(house_mnz$new_surface_mzn))
+table(is.na(train_chapi$new_surface))
+
+## make buffer
+house_buf = st_buffer(train_chapi,dist=20)
+house_buf = st_join(house_buf,train_chapi[,"new_surface"])
+st_geometry(house_buf) = NULL
+house_buf_mean = house_buf %>% group_by(property_id) %>% summarise(surface_new_3=mean(new_surface.y,na.rm=T))
+house_mnz = left_join(house_mnz,house_buf_mean,"property_id")
+
+train_chapi = train_chapi %>% mutate(new_surface = ifelse(is.na(new_surface)==T,house_mnz$surface_new_3,new_surface))
+table(is.na(train_chapi$new_surface))
+
+train_chapi = train_chapi %>% mutate(new_surface = ifelse(is.na(new_surface)==T,mean(train_chapi[is.na(train_chapi$new_surface)==F,]$new_surface),new_surface))
+table(is.na(train_chapi$new_surface))
+table(is.na(train_chapi$property_type))
+
+##Prediccion si tiene balcon / terraza
+p1 = "balcon" ## pattern
+p2 = "balcón" ## pattern
+p3 = "terraza" ## pattern
+
+train_chapi$balcon<-NULL
+
+train_chapi = train_chapi %>% 
+  mutate(balcon = str_extract(string=train_chapi$new_desc , pattern= p1))
+train_chapi = train_chapi %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=train_chapi$new_desc , pattern= p2),balcon))
+train_chapi = train_chapi %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=train_chapi$new_desc , pattern= p3),balcon))
+
+train_chapi = train_chapi %>% mutate(balcon_terr = ifelse(is.na(balcon)==T, 0 ,1))
+
+
+train_chapi <- train_chapi %>%
+  mutate_at(.vars = c(
+    "balcon_terr"),
+    .funs = factor)
+
+#Solo Medellín-----------------------
+train_med<-subset(train_final,l3 %in% c("Medellín"))
+
+mz_med<-st_read(here(path,"05_ANTIOQUIA/URBANO/MGN_URB_MANZANA.shp"))
+
+
+poblado = getbb(place_name = "Comuna 14 - El Poblado, Medellín",
+                featuretype = "boundary:administrative",
+                format_out = "sf_polygon") 
+
+mz_pob = mz_med[poblado,]
+
+
+##Train Poblado muy pocas observaciones, se realiza con todo Medellín
+
+
+## Prediccion de superficie a partir de la descripcion
+
+table(is.na(train_med$surface_total))
+table(is.na(train_med$surface_covered))
+train_med$new_surface<-NULL
+train_med = train_med %>% mutate(new_surface=surface_total)
+table(is.na(train_med$new_surface))
+train_med = train_med %>% mutate(new_surface=ifelse(is.na(new_surface)==T,surface_covered,new_surface))
+table(is.na(train_med$new_surface))
+
+
+train_med = train_med %>% mutate(new_desc=str_to_lower(description))
+
+
+p1 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m" ## pattern
+p2 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m2" ## pattern
+
+train_med$new_surface2<-NA
+
+train_med = train_med %>% 
+  mutate(new_surface2 = str_extract(string=train_med$new_desc , pattern= p1))
+train_med = train_med %>% 
+  mutate(new_surface2 = ifelse(is.na(new_surface2)==T,
+                               str_extract(string=train_med$new_desc , pattern= p2),
+                               new_surface2))
+train_med$new_surface3<-NA
+
+train_med = train_med %>% 
+  mutate(new_surface3 = str_extract(string=train_med$new_surface2 , pattern= "[:digit:]{2,3}"))
+train_med$new_surface3 <- as.numeric(train_med$new_surface3)
+train_med = train_med %>% mutate(new_surface3 = ifelse(new_surface3<=40,NA,new_surface3))
+
+train_med = train_med %>% mutate(new_surface = ifelse(is.na(new_surface)==T,new_surface3,new_surface))
+table(is.na(train_med$new_surface))
+
+
+## make buffer
+house_buf<-NULL
+house_buf_mean<-NULL
+house_buf = st_buffer(train_med,dist=100)
+house_buf = st_join(house_buf,train_med[,"new_surface"])
+st_geometry(house_buf) = NULL
+house_buf_mean = house_buf %>% group_by(property_id) %>% summarise(surface_new_3=mean(new_surface.y,na.rm=T))
+train_med = left_join(train_med,house_buf_mean,"property_id")
+
+train_med = train_med %>% mutate(new_surface = ifelse(is.na(new_surface)==T,train_med$surface_new_3,new_surface))
+table(is.na(train_med$new_surface))
+
+train_med = train_med %>% mutate(new_surface = ifelse(is.na(new_surface)==T,mean(train_med[is.na(train_med$new_surface)==F,]$new_surface),new_surface))
+table(is.na(train_med$new_surface))
+table(is.na(train_med$property_type))
+
+
+##Prediccion si tiene balcon / terraza
+p1 = "balcon" ## pattern
+p2 = "balcón" ## pattern
+p3 = "terraza" ## pattern
+
+train_med$balcon<-NULL
+
+train_med = train_med %>% 
+  mutate(balcon = str_extract(string=train_med$new_desc , pattern= p1))
+train_med = train_med %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=train_med$new_desc , pattern= p2),balcon))
+train_med = train_med %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=train_med$new_desc , pattern= p3),balcon))
+
+train_med = train_med %>% mutate(balcon_terr = ifelse(is.na(balcon)==T, 0 ,1))
+
+
+train_med <- train_med %>%
+  mutate_at(.vars = c(
+    "balcon_terr"),
+    .funs = factor)
+
+
+##Predicciones para Test
+
+#Para Test Chapinero
+mz_bog<-st_read(here(path,"11_BOGOTA/URBANO/MGN_URB_MANZANA.shp"))
+
+
+chapinero = getbb(place_name = "UPZ Chapinero, Bogota",
+                  featuretype = "boundary:administrative",
+                  format_out = "sf_polygon") %>% .$multipolygon
+
+test_chapi<-test_bog
+
+leaflet() %>% addTiles()%>%addCircleMarkers(data=test_chapi, col="red") %>% 
+  addPolygons(data=mz_chapi) 
+
+## Prediccion de superficie a partir de la descripcion
+
+table(is.na(test_chapi$surface_total))
+table(is.na(test_chapi$surface_covered))
+
+test_chapi = test_chapi %>% mutate(new_surface=surface_total)
+test_chapi = test_chapi %>% mutate(new_surface=ifelse(is.na(new_surface)==T,surface_covered,new_surface))
+
+table(is.na(test_chapi$new_surface))
+
+test_chapi = test_chapi %>% mutate(new_desc=str_to_lower(description))
+
+
+p1 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m" ## pattern
+p2 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m2" ## pattern
+
+test_chapi$new_surface2<-NA
+
+test_chapi = test_chapi %>% 
+  mutate(new_surface2 = str_extract(string=test_chapi$new_desc , pattern= p1))
+test_chapi = test_chapi %>% 
+  mutate(new_surface2 = ifelse(is.na(new_surface2)==T,
+                               str_extract(string=test_chapi$new_desc , pattern= p2),
+                               new_surface2))
+test_chapi$new_surface3<-NA
+test_chapi = test_chapi %>% 
+  mutate(new_surface3 = str_extract(string=test_chapi$new_surface2 , pattern= "[:digit:]{2,3}"))
+test_chapi$new_surface3 <- as.numeric(test_chapi$new_surface3)
+test_chapi = test_chapi %>% mutate(new_surface3 = ifelse(new_surface3<=40,NA,new_surface3))
+
+test_chapi = test_chapi %>% mutate(new_surface = ifelse(is.na(new_surface)==T,new_surface3,new_surface))
+table(is.na(test_chapi$new_surface))
+
+## Prediccion de superficie a partir de las manzanas
+
+house_mnz = st_join(x = test_chapi,y = mz_chapi)
+colnames(house_mnz)
+house_mnz = house_mnz %>%
+  group_by(MANZ_CCNCT) %>%
+  mutate(new_surface_mzn=median(new_surface,na.rm=T))
+
+test_chapi = test_chapi %>% mutate(new_surface = ifelse(is.na(new_surface)==T,house_mnz$new_surface_mzn,new_surface))
+
+table(is.na(house_mnz$new_surface_mzn))
+table(is.na(test_chapi$new_surface))
+
+## make buffer
+house_buf = st_buffer(test_chapi,dist=20)
+house_buf = st_join(house_buf,test_chapi[,"new_surface"])
+st_geometry(house_buf) = NULL
+house_buf_mean = house_buf %>% group_by(property_id) %>% summarise(surface_new_3=mean(new_surface.y,na.rm=T))
+house_mnz = left_join(house_mnz,house_buf_mean,"property_id")
+
+test_chapi = test_chapi %>% mutate(new_surface = ifelse(is.na(new_surface)==T,house_mnz$surface_new_3,new_surface))
+table(is.na(test_chapi$new_surface))
+
+test_chapi = test_chapi %>% mutate(new_surface = ifelse(is.na(new_surface)==T,mean(test_chapi[is.na(test_chapi$new_surface)==F,]$new_surface),new_surface))
+table(is.na(test_chapi$new_surface))
+table(is.na(test_chapi$property_type))
+
+##Prediccion si tiene balcon / terraza
+p1 = "balcon" ## pattern
+p2 = "balcón" ## pattern
+p3 = "terraza" ## pattern
+
+test_chapi$balcon<-NULL
+
+test_chapi = test_chapi %>% 
+  mutate(balcon = str_extract(string=test_chapi$new_desc , pattern= p1))
+test_chapi = test_chapi %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=test_chapi$new_desc , pattern= p2),balcon))
+test_chapi = test_chapi %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=test_chapi$new_desc , pattern= p3),balcon))
+
+test_chapi = test_chapi %>% mutate(balcon_terr = ifelse(is.na(balcon)==T, 0 ,1))
+
+
+test_chapi <- test_chapi %>%
+  mutate_at(.vars = c(
+    "balcon_terr"),
+    .funs = factor)
+
+#Para Test Medellin
+
+test_med<-subset(test_final,l3 %in% c("Medellín"))
+
+mz_med<-st_read(here(path,"05_ANTIOQUIA/URBANO/MGN_URB_MANZANA.shp"))
+
+
+poblado = getbb(place_name = "Comuna 14 - El Poblado, Medellín",
+                featuretype = "boundary:administrative",
+                format_out = "sf_polygon") 
+
+mz_pob = mz_med[poblado,]
+
+leaflet() %>% addTiles()%>%addCircleMarkers(data=test_med, col="red") %>% 
+  addPolygons(data=poblado) 
+
+
+## Prediccion de superficie a partir de la descripcion
+
+table(is.na(test_med$surface_total))
+table(is.na(test_med$surface_covered))
+test_med$new_surface<-NULL
+test_med = test_med %>% mutate(new_surface=surface_total)
+table(is.na(test_med$new_surface))
+test_med = test_med %>% mutate(new_surface=ifelse(is.na(new_surface)==T,surface_covered,new_surface))
+table(is.na(test_med$new_surface))
+
+
+test_med = test_med %>% mutate(new_desc=str_to_lower(description))
+
+
+p1 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m" ## pattern
+p2 = "[:space:]+[:digit:]{2,3}+[:punct:]+[:digit:]{1,2}+[:space:]+m2" ## pattern
+
+test_med$new_surface2<-NA
+
+test_med = test_med %>% 
+  mutate(new_surface2 = str_extract(string=test_med$new_desc , pattern= p1))
+test_med = test_med %>% 
+  mutate(new_surface2 = ifelse(is.na(new_surface2)==T,
+                               str_extract(string=test_med$new_desc , pattern= p2),
+                               new_surface2))
+test_med$new_surface3<-NA
+
+test_med = test_med %>% 
+  mutate(new_surface3 = str_extract(string=test_med$new_surface2 , pattern= "[:digit:]{2,3}"))
+test_med$new_surface3 <- as.numeric(test_med$new_surface3)
+test_med = test_med %>% mutate(new_surface3 = ifelse(new_surface3<=40,NA,new_surface3))
+
+test_med = test_med %>% mutate(new_surface = ifelse(is.na(new_surface)==T,new_surface3,new_surface))
+table(is.na(test_med$new_surface))
+
+
+## make buffer
+house_buf<-NULL
+house_buf_mean<-NULL
+house_buf = st_buffer(test_med,dist=100)
+house_buf = st_join(house_buf,test_med[,"new_surface"])
+st_geometry(house_buf) = NULL
+house_buf_mean = house_buf %>% group_by(property_id) %>% summarise(surface_new_3=mean(new_surface.y,na.rm=T))
+test_med = left_join(test_med,house_buf_mean,"property_id")
+
+test_med = test_med %>% mutate(new_surface = ifelse(is.na(new_surface)==T,test_med$surface_new_3,new_surface))
+table(is.na(test_med$new_surface))
+
+test_med = test_med %>% mutate(new_surface = ifelse(is.na(new_surface)==T,mean(test_med[is.na(test_med$new_surface)==F,]$new_surface),new_surface))
+table(is.na(test_med$new_surface))
+table(is.na(test_med$property_type))
+
+
+##Prediccion si tiene balcon / terraza
+p1 = "balcon" ## pattern
+p2 = "balcón" ## pattern
+p3 = "terraza" ## pattern
+
+test_med$balcon<-NULL
+
+test_med = test_med %>% 
+  mutate(balcon = str_extract(string=test_med$new_desc , pattern= p1))
+test_med = test_med %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=test_med$new_desc , pattern= p2),balcon))
+test_med = test_med %>% 
+  mutate(balcon = ifelse(is.na(balcon)==T,
+                         str_extract(string=test_med$new_desc , pattern= p3),balcon))
+
+test_med = test_med %>% mutate(balcon_terr = ifelse(is.na(balcon)==T, 0 ,1))
+
+
+test_med <- test_med %>%
+  mutate_at(.vars = c(
+    "balcon_terr"),
+    .funs = factor)
+
+table(is.na(test_chapi$new_surface))
+table(is.na(test_chapi$bedrooms))
+table(is.na(test_chapi$min_dist_bus))
+table(is.na(test_chapi$min_dist_market))
+table(is.na(test_chapi$property_type))
+table(is.na(train_chapi$new_surface))
+table(is.na(train_chapi$bedrooms))
+table(is.na(train_chapi$min_dist_bus))
+table(is.na(train_chapi$min_dist_market))
+table(is.na(train_chapi$property_type))
+table(is.na(train_chapi$price))
+table(is.na(test_med$new_surface))
+table(is.na(test_med$bedrooms))
+table(is.na(test_med$min_dist_bus))
+table(is.na(test_med$min_dist_market))
+table(is.na(test_med$property_type))
+table(is.na(train_med$new_surface))
+table(is.na(train_med$bedrooms))
+table(is.na(train_med$min_dist_bus))
+table(is.na(train_med$min_dist_market))
+table(is.na(train_med$property_type))
+table(is.na(train_med$price))
+
+save(test_chapi,train_chapi,test_med,train_med, file = "data_pred.RData")
+load("stuff.RData")
